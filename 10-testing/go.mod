6a8f33a36b17e406e8834d6644e7ed90957f93c2
Jesus Camacaro <jesus.alejandr@gmail.com>
2022-04-10 18:15:46 -0400

    init testing

---
+++ 10-testing/go.mod
@@ -0,0 +1,3 @@
+module github.com/Camacaro/testing
+
+go 1.18
---
+++ 10-testing/main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	total := Sum(5, 5)
+
+	if total != 10 {
+		t.Errorf("Sum(5, 5) failed. Got %d, expected %d", total, 10)
+		t.Error("El test fallo")
+	}
+
+	tables := []struct {
+		a int
+		b int
+		n int
+	}{
+		{1, 2, 3},
+		{2, 3, 5},
+		{3, 4, 7},
+	}
+
+	for _, table := range tables {
+		total := Sum(table.a, table.b)
+
+		if total != table.n {
+			t.Errorf("Sum(%d, %d) failed. Got %d, expected %d", table.a, table.b, total, table.n)
+		}
+	}
+}
+
+func TestGetMax(t *testing.T) {
+	tables := []struct {
+		firstValue     int
+		secondValue    int
+		resultExpected int
+	}{
+		{1, 2, 2},
+		{2, 3, 3},
+		{5, 4, 5},
+	}
+
+	for _, table := range tables {
+		result := GetMax(table.firstValue, table.secondValue)
+
+		if result != table.resultExpected {
+			t.Errorf("GetMax(%d, %d) failed. Got %d, expected %d", table.firstValue, table.secondValue, result, table.resultExpected)
+		}
+	}
+}
